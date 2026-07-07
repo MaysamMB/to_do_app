10cf09f06567621ed00060b23d36d117d680c758
Maysam Baradiya <maysammbaradeah@gmail.com>
2026-07-07 21:52:07 +0300

    add mock data to file mock_tasks in folder data

---
+++ lib/data/mock_tasks.dart
@@ -0,0 +1,46 @@
+import '../models/tasks.dart';
+
+const List<Task> mockTasks = [
+  Task(
+    id: '1',
+    title: 'Study Flutter',
+    description: 'Finish learning Flutter widgets and layouts.',
+    isCompleted: false,
+  ),
+  Task(
+    id: '2',
+    title: 'Complete Dart Exercises',
+    description: 'Solve practice problems on functions and collections.',
+    isCompleted: true,
+  ),
+  Task(
+    id: '3',
+    title: 'Read Riverpod Documentation',
+    description: 'Understand the basics of state management.',
+    isCompleted: false,
+  ),
+  Task(
+    id: '4',
+    title: 'Workout',
+    description: 'Go to the gym for one hour.',
+    isCompleted: true,
+  ),
+  Task(
+    id: '5',
+    title: 'Buy Groceries',
+    description: 'Milk, eggs, bread, and fruits.',
+    isCompleted: false,
+  ),
+  Task(
+    id: '6',
+    title: 'Prepare Presentation',
+    description: 'Finish the slides for the Flutter project.',
+    isCompleted: false,
+  ),
+  Task(
+    id: '7',
+    title: 'Train for Presentation',
+    description: 'Finish Train for the Flutter project Presentation.',
+    isCompleted: false,
+  ),
+];
