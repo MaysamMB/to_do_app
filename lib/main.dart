af6eeec8cc72b7f96ff3e053900c82248520cde7
Maysam Baradiya <maysammbaradeah@gmail.com>
2026-07-09 09:38:30 +0300

    Set HomeScreen as the home page

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'To Do App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
