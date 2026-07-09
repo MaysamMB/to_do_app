3fc6e0c60d814edf80f4fdd1a52c6f33064065d8
Maysam Baradiya <maysammbaradeah@gmail.com>
2026-07-09 15:42:12 +0300

    Add task card actions and completion UI

--- lib/widgets/task_card.dart
+++ lib/widgets/task_card.dart
@@ -5,7 +5,15 @@ import '../models/tasks.dart';
 
 class TaskCard extends StatelessWidget {
   final Task task;
-  const TaskCard({super.key, required this.task});
+  final VoidCallback onComplete;
+  final VoidCallback onDetails;
+
+  const TaskCard({
+    super.key,
+    required this.task,
+    required this.onComplete,
+    required this.onDetails,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -13,18 +21,46 @@ class TaskCard extends StatelessWidget {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       child: ListTile(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        enabled: task.isCompleted ? true : false,
+        enabled: !task.isCompleted,
 
         tileColor: task.isCompleted
             ? const Color(0xFFFFE2E2)
             : const Color(0xFFFFFFFF),
-        leading: Text(task.id.toString(), style: TextStyle()),
+        // leading: Text(task.id.toString(), style: TextStyle()),
+        leading: GestureDetector(
+          onTap: onComplete,
+          child: Container(
+            width: 25,
+            height: 25,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              border: Border.all(color: const Color(0xFFC5B3D3), width: 2),
+              color: task.isCompleted
+                  ? const Color(0xFFC5B3D3)
+                  : Colors.transparent,
+            ),
+            child: task.isCompleted
+                ? const Icon(Icons.check, size: 18, color: Colors.white)
+                : null,
+          ),
+        ),
+        trailing: IconButton(
+          onPressed: onDetails,
+          icon: const Icon(Icons.arrow_forward_ios),
+        ),
         title: Text(
           task.title,
           style: TextStyle(
             color: const Color(0xFFC5B3D3),
             fontSize: 20,
             fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text(
+          "Priority: ${task.priority}",
+          style: TextStyle(
+            color: Color.fromARGB(255, 187, 175, 196),
+            fontWeight: FontWeight.w600,
           ),
         ),
       ),
