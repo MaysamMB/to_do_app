97fbef03439f30af5e3f6d1efe844b089b904fbe
Maysam Baradiya <maysammbaradeah@gmail.com>
2026-07-30 11:13:28 +0300

    Add serialization methods to Task model for JSON and Map conversion

--- lib/models/tasks.dart
+++ lib/models/tasks.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Task {
   final String id;
   final String title;
@@ -32,4 +34,39 @@ class Task {
       priority: priority ?? this.priority,
     );
   }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'isCompleted': isCompleted,
+      'name': name,
+      'priority': priority,
+    };
+  }
+
+  Map<String, dynamic> toApiMap() {
+    return {
+      'todo': title,
+      'completed': isCompleted,
+      'userId': 5, 
+    };
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+  return Task(
+    id: map['id'].toString(),
+    title: map['todo'] ?? map['title'] ?? '',
+    description: map['description'] ?? 'No description provided.',
+    isCompleted: (map['completed'] ?? map['isCompleted'] ?? false) as bool,
+    name: map['name'] as String? ?? '',
+    priority: map['priority'] ?? 'Mid',
+  );
+}
+
+  String toJson() => jsonEncode(toMap());
+
+  factory Task.fromJson(String source) =>
+      Task.fromMap(jsonDecode(source) as Map<String, dynamic>);
 }
